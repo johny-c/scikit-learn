fdb91ec05d93ae8b825190a978488b99fbdb1abf
John Chiotellis <chiotell@in.tum.de>
2017-10-24 23:38:28 +0200

    add latest commit from numpy 1.13.X

--- sklearn/utils/fixes.py
+++ sklearn/utils/fixes.py
@@ -195,52 +195,100 @@ else:
     from numpy.ma import MaskedArray    # noqa
 
 
-if np_version < (1, 9, 0):
-    # Allow unique to return counts
-    # https://github.com/numpy/numpy/commit/09fb4205a1d56090e13257a181f23514684f532b
+# Allow unique to return counts and
+# Added axis argument to numpy.unique
+# https://github.com/numpy/numpy/commit/1f764dbff7c496d6636dc0430f083ada9ff4e4be
 
-    def unique(ar, return_index=False, return_inverse=False,
-               return_counts=False):
-        ar = np.asanyarray(ar).flatten()
+def unique(ar, return_index=False, return_inverse=False,
+           return_counts=False, axis=None):
+    ar = np.asanyarray(ar)
+    if axis is None:
+        return _unique1d(ar, return_index, return_inverse, return_counts)
+    if not (-ar.ndim <= axis < ar.ndim):
+        raise ValueError('Invalid axis kwarg specified for unique')
 
-        optional_indices = return_index or return_inverse
-        optional_returns = optional_indices or return_counts
+    ar = np.swapaxes(ar, axis, 0)
+    orig_shape, orig_dtype = ar.shape, ar.dtype
+    # Must reshape to a contiguous 2D array for this to work...
+    ar = ar.reshape(orig_shape[0], -1)
+    ar = np.ascontiguousarray(ar)
 
-        if ar.size == 0:
-            if not optional_returns:
-                ret = ar
-            else:
-                ret = (ar,)
-                if return_index:
-                    ret += (np.empty(0, np.bool),)
-                if return_inverse:
-                    ret += (np.empty(0, np.bool),)
-                if return_counts:
-                    ret += (np.empty(0, np.intp),)
-            return ret
+    if ar.dtype.char in (np.typecodes['AllInteger'] +
+                             np.typecodes['Datetime'] + 'S'):
+        # Optimization: Creating a view of your data with a np.void data type of
+        # size the number of bytes in a full row. Handles any type where items
+        # have a unique binary representation, i.e. 0 is only 0, not +0 and -0.
+        dtype = np.dtype((np.void, ar.dtype.itemsize * ar.shape[1]))
+    else:
+        dtype = [('f{i}'.format(i=i), ar.dtype) for i in
+                 range(ar.shape[1])]
 
-        if optional_indices:
-            perm = ar.argsort(
-                kind='mergesort' if return_index else 'quicksort')
-            aux = ar[perm]
-        else:
-            ar.sort()
-            aux = ar
-        flag = np.concatenate(([True], aux[1:] != aux[:-1]))
+    try:
+        consolidated = ar.view(dtype)
+    except TypeError:
+        # There's no good way to do this for object arrays, etc...
+        msg = 'The axis argument to unique is not supported for dtype {dt}'
+        raise TypeError(msg.format(dt=ar.dtype))
 
+    def reshape_uniq(uniq):
+        uniq = uniq.view(orig_dtype)
+        uniq = uniq.reshape(-1, *orig_shape[1:])
+        uniq = np.swapaxes(uniq, 0, axis)
+        return uniq
+
+    output = _unique1d(consolidated, return_index,
+                       return_inverse, return_counts)
+    if not (return_index or return_inverse or return_counts):
+        return reshape_uniq(output)
+    else:
+        uniq = reshape_uniq(output[0])
+        return (uniq,) + output[1:]
+
+
+def _unique1d(ar, return_index=False, return_inverse=False,
+              return_counts=False):
+    """
+    Find the unique elements of an array, ignoring shape.
+    """
+    ar = np.asanyarray(ar).flatten()
+
+    optional_indices = return_index or return_inverse
+    optional_returns = optional_indices or return_counts
+
+    if ar.size == 0:
         if not optional_returns:
-            ret = aux[flag]
+            ret = ar
         else:
-            ret = (aux[flag],)
+            ret = (ar,)
             if return_index:
-                ret += (perm[flag],)
+                ret += (np.empty(0, np.bool),)
             if return_inverse:
-                iflag = np.cumsum(flag) - 1
-                iperm = perm.argsort()
-                ret += (np.take(iflag, iperm),)
+                ret += (np.empty(0, np.bool),)
             if return_counts:
-                idx = np.concatenate(np.nonzero(flag) + ([ar.size],))
-                ret += (np.diff(idx),)
+                ret += (np.empty(0, np.intp),)
         return ret
-else:
-    from numpy import unique    # noqa
+
+    if optional_indices:
+        perm = ar.argsort(
+            kind='mergesort' if return_index else 'quicksort')
+        aux = ar[perm]
+    else:
+        ar.sort()
+        aux = ar
+    flag = np.concatenate(([True], aux[1:] != aux[:-1]))
+
+    if not optional_returns:
+        ret = aux[flag]
+    else:
+        ret = (aux[flag],)
+        if return_index:
+            ret += (perm[flag],)
+        if return_inverse:
+            iflag = np.cumsum(flag) - 1
+            inv_idx = np.empty(ar.shape, dtype=np.intp)
+            inv_idx[perm] = iflag
+            ret += (inv_idx,)
+        if return_counts:
+            idx = np.concatenate(np.nonzero(flag) + ([ar.size],))
+            ret += (np.diff(idx),)
+    return ret
